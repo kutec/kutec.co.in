b45da4ead770d227ed0a17e8c4b262090f6f85f6
Kushal Jayswal <ku@kutec.co.in>
2019-08-30 00:07:27 +0530

    First Commit

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Kushal Jayswal', // Site title.
+  siteTitleAlt: 'Fronend Developer', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://kutec.co.in', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/kutec.co.in /.
+  siteDescription: 'Kushal Jayswal, a professional frontend developer from India.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: '', // Disqus shortname.
+  userName: '',
+  userTwitter: 'kutec',
+  userLocation: 'Ahmedabad, Gujarat, India',
+  userDescription: '',
+  copyright: 'Copyright © Kushal Jayswal and Friends 2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
