f7280b303747b006b2c178f08777af96491efa72
Kushal Jayswal <jayswal.kushal+github@gmail.com>
2025-01-03 22:45:18 +0530

    Fix the build

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
 import config from "../../data/SiteConfig";
 // import 'bulma/css/bulma.css';
 // import "./index.scss";
@@ -7,7 +8,6 @@ import Header from "../components/Header/Header";
 import SiteFooter from "../components/SiteFooter/SiteFooter";
 
 import "../assets/css/main.scss";
-import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader";
 
 export default function MainLayout({ children }) {
   deckDeckGoHighlightElement();
